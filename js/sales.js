15894e332ba8590e90902bb623490214e4cbd9dd
Ibrahim Sule <sulayibraheem@gmail.com>
2020-08-21 00:00:31 +0100

    added css html and js files

---
+++ js/sales.js
@@ -0,0 +1,8 @@
+let totalPrice = document.getElementsByTagName('input')[0].value;
+let balanceReturn= document.getElementsByTagName('input')[1].value;
+let itemPrice= document.getElementsByTagName('input')[2].value;
+let itemPurchased= document.getElementsByTagName('input')[3].value;
+let amountPaid= document.getElementsByTagName('input')[4].value;
+let add= document.getElementsByTagName('button')[0];
+let remove= document.getElementsByTagName('button')[1];
+let submit= document.getElementsByTagName('button')[2];
