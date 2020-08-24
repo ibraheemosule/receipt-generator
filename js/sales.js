98e1425d852f02c0237ca784e7f42d68fa0fc99e
Ibrahim Sule <sulayibraheem@gmail.com>
2020-08-24 11:09:43 +0100

    completed the receipt table and submit button

--- js/sales.js
+++ js/sales.js
@@ -14,7 +14,7 @@ let totalPrice = document.getElementsByTagName('input')[0];
 let balance = document.getElementsByTagName('input')[1];
 let product = document.getElementsByTagName('input')[2];
 let OutputProduct = document.getElementsByTagName('input')[2].value;
- let newPrice = document.getElementsByTagName('input')[0].value;
+let newPrice = document.getElementsByTagName('input')[0].value;
 let productPrice= document.getElementsByTagName('input')[3];
 let numberPurchased= document.getElementsByTagName('input')[4];
 let amountPaid= document.getElementsByTagName('input')[5];
@@ -91,7 +91,96 @@ if(newPrice.includes("+", 1) && productPrice.value !== "" && numberPurchased.val
 
    table.appendChild(finalTableRow);
 
-   console.log("this is you");
+   product.value = ""
+   productPrice.value = ""
+   numberPurchased.value = ""
+
+   product.style.pointerEvents = "none";
+   productPrice.style.pointerEvents = "none";
+   numberPurchased.style.pointerEvents = "none";
+   amountPaid.style.pointerEvents = "none";
+
+   add.disabled = true;
+   submit.disabled = true;
+   
+    };
+} else {
+
+    let price = parseInt(newPrice);
+    let newProductPrice = parseInt(productPrice.value);
+    let newNumberPurchased = parseInt(numberPurchased.value);
+    let newAmountPaid = parseInt(amountPaid.value);
+
+    if(newAmountPaid === "" || newAmountPaid === NaN ){
+document.getElementsByTagName('input')[5].placeholder = "ENTER AMOUNT PAID";
+} else if(newNumberPurchased === "" || newNumberPurchased === NaN){
+    document.getElementsByTagName('input')[4].placeholder = "Input a Number of item";
+}  else if(newProductPrice === "" || newProductPrice === NaN ){
+    document.getElementsByTagName('input')[4].placeholder = "Input a Number of item";
+}  else {
+    let answer = newProductPrice * newNumberPurchased;
+    let itemPrice = newProductPrice * newNumberPurchased;
+
+    totalPrice.value = answer;
+
+    if (isNaN(newAmountPaid) === false && newAmountPaid !== "") {
+        let balanceReturn = newAmountPaid - answer;
+        if(balanceReturn > 0){
+            balance.value = balanceReturn;
+        } else {
+            balance.value = "NO BALANCE";
+        }
+    };
+    
+    let table = document.getElementsByTagName("table")[0];
+
+    let tableRow = document.createElement("tr");
+
+    let columnOne = document.createElement("td");
+    let columnTwo = document.createElement("td");
+    let columnThree = document.createElement("td");
+    let columnFour = document.createElement("td");
+
+    let itemBought = document.createTextNode(OutputProduct);
+    let theItemPrice = document.createTextNode(newProductPrice);
+    let itemNumber = document.createTextNode(newNumberPurchased);
+   let priceItem = document.createTextNode(itemPrice);
+
+   columnOne.appendChild(itemBought);
+   columnTwo.appendChild(theItemPrice);
+   columnThree.appendChild(itemNumber);
+   columnFour.appendChild(priceItem);
+
+   tableRow.appendChild(columnOne);
+   tableRow.appendChild(columnTwo);
+   tableRow.appendChild(columnThree);
+   tableRow.appendChild(columnFour);
+
+   table.appendChild(tableRow);
+
+   let finalTableRow = document.createElement("tr");
+
+   let firstColumnOne = document.createElement("td");
+   let firstColumnTwo = document.createElement("td");
+   let firstColumnThree = document.createElement("td");
+   let firstColumnFour = document.createElement("td");
+
+   let balanceValue = document.createTextNode("BALANCE:");
+   let thePrice = document.createTextNode(balance.value);
+   let totalPriceValue = document.createTextNode("TOTAL PRICE:");
+  let thePriceItem = document.createTextNode(totalPrice.value);
+
+   firstColumnOne.appendChild(balanceValue);
+   firstColumnTwo.appendChild(thePrice);
+   firstColumnThree.appendChild(totalPriceValue);
+   firstColumnFour.appendChild(thePriceItem);
+
+   finalTableRow.appendChild(firstColumnOne);
+   finalTableRow.appendChild(firstColumnTwo);
+   finalTableRow.appendChild(firstColumnThree);
+   finalTableRow.appendChild(firstColumnFour);
+
+   table.appendChild(finalTableRow);
 
    product.value = ""
    productPrice.value = ""
@@ -101,28 +190,12 @@ if(newPrice.includes("+", 1) && productPrice.value !== "" && numberPurchased.val
    productPrice.style.pointerEvents = "none";
    numberPurchased.style.pointerEvents = "none";
    amountPaid.style.pointerEvents = "none";
+
+   add.disabled = true;
+   submit.disabled = true;
    
     };
-} /*else {
-    if(newPrice === ""){
-document.getElementsByTagName('input')[3].placeholder = "Input Price of item";
-} else if(numberPurchased === ""){
-    document.getElementsByTagName('input')[4].placeholder = "Input a Number of item";
-}  else if(isNaN(itemPrice)== true || isNaN(numberPurchased)== true){
-    document.getElementsByTagName('input')[0].placeholder = "Invalid result"
-} else if(isNaN(itemPrice)== false && isNaN(numberPurchased)== false){
-let totalPrice = document.getElementsByTagName('input')[0];
-let answer = eval(itemPrice * numberPurchased);
- totalPrice.value = answer;
-
- if (isNaN(amountPaid) == false && amountPaid !== "") {
-    let balance = eval(amountPaid - answer);
-    if(balance > 0){
-        balanceReturn.value = balance;
-    };
- };
 };
-}; */
 };
 submit.addEventListener('click', totalPriceFunc);
 
@@ -197,9 +270,6 @@ const addition = () => {
             };
         };
 
-    
-       //WORKS 
-
        let table = document.getElementsByTagName("table")[0];
 
        let tableRow = document.createElement("tr");
@@ -230,9 +300,6 @@ const addition = () => {
        document.getElementsByTagName('input')[3].value = "";
        document.getElementsByTagName('input')[4].value = "";
 
-
-
-
     } else {
         alert("ENTER VALID DIGITS");
     };
@@ -248,5 +315,6 @@ const set = () => {
     document.getElementsByTagName('input')[3].value = "";
     document.getElementsByTagName('input')[4].value = "";
     document.getElementsByTagName('input')[5].value = "";
+    document.location.reload();
 };
 reset.addEventListener("click", set);
